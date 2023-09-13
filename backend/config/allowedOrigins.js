3e2e32d7fa4fbe1e6cf6d035b41a339a284dadb7
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-09-13 17:40:32 +0100

    updating be

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://authorisation-authentication-akfo3f2ku-vince4559.vercel.app']; //'http://localhost:5173'
 
 module.exports = allowedOrigins
